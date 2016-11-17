208dfbc822f3dcb1691b0bf4512ac6e335fe1d37
ahpho <kuangsihao@gmail.com>
2016-11-17 14:37:55 +0800

    添加glfw并测试wave例子成功  http://www.glfw.org/

--- TestGL/TestGL.cpp
+++ TestGL/TestGL.cpp
@@ -1,157 +1,470 @@
 #include "stdafx.h"
-#pragma comment(lib,"glew32.lib")//这里要加载该静态包
-#include <GL/glew.h>
-#include <GL/glut.h>
-#include <glm\glm.hpp>//斜杠和反斜杠都可以
-#include <iostream>
+// #pragma comment(lib,"glew32.lib")//这里要加载该静态包
+// #include <GL/glew.h>
+// #include <GL/glut.h>
+// #include <glm\glm.hpp>//斜杠和反斜杠都可以
+// #include <iostream>
 
-/*
-完整的shader过程。
-初始化→编译→链接到程序
+/*****************************************************************************
+* Wave Simulation in OpenGL
+* (C) 2002 Jakob Thomsen
+* http://home.in.tum.de/~thomsen
+* Modified for GLFW by Sylvain Hellegouarch - sh@programmationworld.com
+* Modified for variable frame rate by Marcus Geelnard
+* 2003-Jan-31: Minor cleanups and speedups / MG
+* 2010-10-24: Formatting and cleanup - Camilla Berglund
+*****************************************************************************/
+
+#if defined(_MSC_VER)
+// Make MS math.h define M_PI
+#define _USE_MATH_DEFINES
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <linmath.h>
+#if defined(_DEBUG) || defined(DEBUG)
+	#pragma comment(lib,"glfw3d.lib")
+#else
+	#pragma comment(lib,"glfw3.lib")
+#endif
+
+// Maximum delta T to allow for differential calculations
+#define MAX_DELTA_T 0.01
+
+// Animation speed (10.0 looks good)
+#define ANIMATION_SPEED 10.0
+
+GLfloat alpha = 210.f, beta = -70.f;
+GLfloat zoom = 2.f;
+
+double cursorX;
+double cursorY;
+
+struct Vertex
+{
+	GLfloat x, y, z;
+	GLfloat r, g, b;
+};
+
+#define GRIDW 50
+#define GRIDH 50
+#define VERTEXNUM (GRIDW*GRIDH)
+
+#define QUADW (GRIDW - 1)
+#define QUADH (GRIDH - 1)
+#define QUADNUM (QUADW*QUADH)
+
+GLuint quad[4 * QUADNUM];
+struct Vertex vertex[VERTEXNUM];
+
+/* The grid will look like this:
+*
+*      3   4   5
+*      *---*---*
+*      |   |   |
+*      | 0 | 1 |
+*      |   |   |
+*      *---*---*
+*      0   1   2
 */
-using namespace std;
 
-/*传入shader的引用，和GLSL字符串，对shader进行编译*/
-void compileShader(GLint& shader, const GLchar* shaderCode) {
-	const GLchar* codeArray[] = { shaderCode };
-	glShaderSource(shader, 1, codeArray, NULL);
-	glCompileShader(shader);//编译shader
+//========================================================================
+// Initialize grid geometry
+//========================================================================
 
-							/*判断编译后的结果*/
-	GLint result;
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+void init_vertices(void)
+{
+	int x, y, p;
 
-	if (result == GL_FALSE) {
-		cout << "shader编译错误！" << endl;
-		GLint logLen;
-		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLen);
+	// Place the vertices in a grid
+	for (y = 0; y < GRIDH; y++)
+	{
+		for (x = 0; x < GRIDW; x++)
+		{
+			p = y * GRIDW + x;
 
-		if (logLen > 0) {
-			char* log = (char*)malloc(logLen);
-			GLsizei written;
-			glGetShaderInfoLog(shader, logLen, &written, log);
-			cout << log << endl;
-			free(log);
+			vertex[p].x = (GLfloat)(x - GRIDW / 2) / (GLfloat)(GRIDW / 2);
+			vertex[p].y = (GLfloat)(y - GRIDH / 2) / (GLfloat)(GRIDH / 2);
+			vertex[p].z = 0;
 
+			if ((x % 4 < 2) ^ (y % 4 < 2))
+				vertex[p].r = 0.0;
+			else
+				vertex[p].r = 1.0;
+
+			vertex[p].g = (GLfloat)y / (GLfloat)GRIDH;
+			vertex[p].b = 1.f - ((GLfloat)x / (GLfloat)GRIDW + (GLfloat)y / (GLfloat)GRIDH) / 2.f;
 		}
-		exit(1);
+	}
+
+	for (y = 0; y < QUADH; y++)
+	{
+		for (x = 0; x < QUADW; x++)
+		{
+			p = 4 * (y * QUADW + x);
+
+			quad[p + 0] = y       * GRIDW + x;     // Some point
+			quad[p + 1] = y       * GRIDW + x + 1; // Neighbor at the right side
+			quad[p + 2] = (y + 1) * GRIDW + x + 1; // Upper right neighbor
+			quad[p + 3] = (y + 1) * GRIDW + x;     // Upper neighbor
+		}
+	}
+}
+
+double dt;
+double p[GRIDW][GRIDH];
+double vx[GRIDW][GRIDH], vy[GRIDW][GRIDH];
+double ax[GRIDW][GRIDH], ay[GRIDW][GRIDH];
+
+//========================================================================
+// Initialize grid
+//========================================================================
+
+void init_grid(void)
+{
+	int x, y;
+	double dx, dy, d;
+
+	for (y = 0; y < GRIDH; y++)
+	{
+		for (x = 0; x < GRIDW; x++)
+		{
+			dx = (double)(x - GRIDW / 2);
+			dy = (double)(y - GRIDH / 2);
+			d = sqrt(dx * dx + dy * dy);
+			if (d < 0.1 * (double)(GRIDW / 2))
+			{
+				d = d * 10.0;
+				p[x][y] = -cos(d * (M_PI / (double)(GRIDW * 4))) * 100.0;
+			}
+			else
+				p[x][y] = 0.0;
+
+			vx[x][y] = 0.0;
+			vy[x][y] = 0.0;
+		}
+	}
+}
+
+
+//========================================================================
+// Draw scene
+//========================================================================
+
+void draw_scene(GLFWwindow* window)
+{
+	// Clear the color and depth buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// We don't want to modify the projection matrix
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// Move back
+	glTranslatef(0.0, 0.0, -zoom);
+	// Rotate the view
+	glRotatef(beta, 1.0, 0.0, 0.0);
+	glRotatef(alpha, 0.0, 0.0, 1.0);
+
+	glDrawElements(GL_QUADS, 4 * QUADNUM, GL_UNSIGNED_INT, quad);
+
+	glfwSwapBuffers(window);
+}
+
+
+//========================================================================
+// Initialize Miscellaneous OpenGL state
+//========================================================================
+
+void init_opengl(void)
+{
+	// Use Gouraud (smooth) shading
+	glShadeModel(GL_SMOOTH);
+
+	// Switch on the z-buffer
+	glEnable(GL_DEPTH_TEST);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_COLOR_ARRAY);
+	glVertexPointer(3, GL_FLOAT, sizeof(struct Vertex), vertex);
+	glColorPointer(3, GL_FLOAT, sizeof(struct Vertex), &vertex[0].r); // Pointer to the first color
+
+	glPointSize(2.0);
+
+	// Background color is black
+	glClearColor(0, 0, 0, 0);
+}
+
+
+//========================================================================
+// Modify the height of each vertex according to the pressure
+//========================================================================
+
+void adjust_grid(void)
+{
+	int pos;
+	int x, y;
+
+	for (y = 0; y < GRIDH; y++)
+	{
+		for (x = 0; x < GRIDW; x++)
+		{
+			pos = y * GRIDW + x;
+			vertex[pos].z = (float)(p[x][y] * (1.0 / 50.0));
+		}
+	}
+}
+
+
+//========================================================================
+// Calculate wave propagation
+//========================================================================
+
+void calc_grid(void)
+{
+	int x, y, x2, y2;
+	double time_step = dt * ANIMATION_SPEED;
+
+	// Compute accelerations
+	for (x = 0; x < GRIDW; x++)
+	{
+		x2 = (x + 1) % GRIDW;
+		for (y = 0; y < GRIDH; y++)
+			ax[x][y] = p[x][y] - p[x2][y];
+	}
+
+	for (y = 0; y < GRIDH; y++)
+	{
+		y2 = (y + 1) % GRIDH;
+		for (x = 0; x < GRIDW; x++)
+			ay[x][y] = p[x][y] - p[x][y2];
+	}
+
+	// Compute speeds
+	for (x = 0; x < GRIDW; x++)
+	{
+		for (y = 0; y < GRIDH; y++)
+		{
+			vx[x][y] = vx[x][y] + ax[x][y] * time_step;
+			vy[x][y] = vy[x][y] + ay[x][y] * time_step;
+		}
+	}
+
+	// Compute pressure
+	for (x = 1; x < GRIDW; x++)
+	{
+		x2 = x - 1;
+		for (y = 1; y < GRIDH; y++)
+		{
+			y2 = y - 1;
+			p[x][y] = p[x][y] + (vx[x2][y] - vx[x][y] + vy[x][y2] - vy[x][y]) * time_step;
+		}
+	}
+}
+
+
+//========================================================================
+// Print errors
+//========================================================================
+
+static void error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+
+//========================================================================
+// Handle key strokes
+//========================================================================
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (action != GLFW_PRESS)
+		return;
+
+	switch (key)
+	{
+	case GLFW_KEY_ESCAPE:
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+		break;
+	case GLFW_KEY_SPACE:
+		init_grid();
+		break;
+	case GLFW_KEY_LEFT:
+		alpha += 5;
+		break;
+	case GLFW_KEY_RIGHT:
+		alpha -= 5;
+		break;
+	case GLFW_KEY_UP:
+		beta -= 5;
+		break;
+	case GLFW_KEY_DOWN:
+		beta += 5;
+		break;
+	case GLFW_KEY_PAGE_UP:
+		zoom -= 0.25f;
+		if (zoom < 0.f)
+			zoom = 0.f;
+		break;
+	case GLFW_KEY_PAGE_DOWN:
+		zoom += 0.25f;
+		break;
+	default:
+		break;
+	}
+}
+
+
+//========================================================================
+// Callback function for mouse button events
+//========================================================================
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+	if (button != GLFW_MOUSE_BUTTON_LEFT)
+		return;
+
+	if (action == GLFW_PRESS)
+	{
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+		glfwGetCursorPos(window, &cursorX, &cursorY);
 	}
 	else
-	{
-		cout << "shader编译成功！" << endl;
-	}
-}
-void render(GLuint programHandle) {
-	float positionData[] = {
-		-0.8f, -0.8f, 0.0f,
-		0.8f, -0.8f, 0.0f,
-		0.0f, 0.8f, 0.0f
-	};
-	float colorData[] = {
-		1.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 1.0f
-	};
-
-	GLuint vboHandle[2];
-	glGenBuffers(2, vboHandle);
-	GLuint positionbufferHandle = vboHandle[0];
-	GLuint colorBufferHandle = vboHandle[1];
-	glBindBuffer(GL_ARRAY_BUFFER, positionbufferHandle);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), positionData, GL_STATIC_DRAW);//绑定顶点数据
-	glBindBuffer(GL_ARRAY_BUFFER, colorBufferHandle);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), colorData, GL_STATIC_DRAW);//绑定颜色数据
-
-	GLuint vaoHandle;
-	glGenVertexArrays(1, &vaoHandle);
-	glBindVertexArray(vaoHandle);
-	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
-	glBindBuffer(GL_ARRAY_BUFFER, positionbufferHandle);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte*)NULL);
-	glBindBuffer(GL_ARRAY_BUFFER, colorBufferHandle);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (GLubyte*)NULL);
-	glBindVertexArray(vaoHandle);
-	//  绘制三角形
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glFlush();
-
-
-}
-void link() {
-	//判断glew是否能初始化
-	if (glewInit() != GLEW_OK) {
-		cout << "glewInit()失败！" << endl;
-		exit(1);
-	}
-	/*编译vertexShader*/
-	const GLchar* shaderCode = "#version 400 \n in vec3 VertexPosition;in vec3 VertexColor;out vec3 Color;void main() {Color = VertexColor;gl_Position = vec4(VertexPosition, 1.0);}";
-	GLint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	if (vertexShader == 0) {
-		cout << "vertexShader无法获取！" << endl;
-		exit(1);
-	}
-	compileShader(vertexShader, shaderCode);
-
-	/*编译fragmentShader*/
-	const GLchar* fragmentCode = "#version 400 \n in vec3 Color;out vec4 FragColor;void main() {FragColor=vec4(Color,1.0);}";
-	GLint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	if (fragmentShader == 0) {
-		cout << "fragmentShader无法获取！" << endl;
-		exit(1);
-	}
-	compileShader(fragmentShader, fragmentCode);
-
-	/*获得程序programHandle*/
-	GLuint programHandle = glCreateProgram();
-	if (programHandle == 0)
-	{
-		cout << "Program无法获取！" << endl;
-		exit(1);
-
-	}
-	/*将shader链接到programHandle*/
-	glAttachShader(programHandle, vertexShader);
-	glAttachShader(programHandle, fragmentShader);
-	glBindAttribLocation(programHandle, 0, "VertexPosition");
-	glBindAttribLocation(programHandle, 1, "VertexColor");
-	glLinkProgram(programHandle);
-
-	/*判断链接状态*/
-	GLint status;
-	glGetProgramiv(programHandle, GL_LINK_STATUS, &status);
-	if (status == GL_FALSE)
-	{
-		cout << "link失败！" << endl;
-		GLint logLen;
-		glGetProgramiv(programHandle, GL_INFO_LOG_LENGTH, &logLen);
-
-		if (logLen > 0) {
-			char* log = (char*)malloc(logLen);
-			GLsizei written;
-			glGetProgramInfoLog(programHandle, logLen, &written, log);
-			cout << log << endl;
-			free(log);
-
-		}
-		exit(1);
-	}
-	else {
-		cout << "link成功！" << endl;
-		glUseProgram(programHandle);//使用该程序
-		render(programHandle);
-	}
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 }
 
-int main(int argc, char *argv[])
+
+//========================================================================
+// Callback function for cursor motion events
+//========================================================================
+
+void cursor_position_callback(GLFWwindow* window, double x, double y)
 {
-	glutInit(&argc, argv);;
-	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-	glutInitWindowPosition(0, 0);
-	glutInitWindowSize(900, 600);
-	glutCreateWindow("First GLUT Sample");
-	/*link函数是GLSL编写的主体*/
-	glutDisplayFunc(link);
-	glutMainLoop();
-	return 0;
+	if (glfwGetInputMode(window, GLFW_CURSOR) == GLFW_CURSOR_DISABLED)
+	{
+		alpha += (GLfloat)(x - cursorX) / 10.f;
+		beta += (GLfloat)(y - cursorY) / 10.f;
+
+		cursorX = x;
+		cursorY = y;
+	}
+}
+
+
+//========================================================================
+// Callback function for scroll events
+//========================================================================
+
+void scroll_callback(GLFWwindow* window, double x, double y)
+{
+	zoom += (float)y / 4.f;
+	if (zoom < 0)
+		zoom = 0;
+}
+
+
+//========================================================================
+// Callback function for framebuffer resize events
+//========================================================================
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	float ratio = 1.f;
+	mat4x4 projection;
+
+	if (height > 0)
+		ratio = (float)width / (float)height;
+
+	// Setup viewport
+	glViewport(0, 0, width, height);
+
+	// Change to the projection matrix and set our viewing volume
+	glMatrixMode(GL_PROJECTION);
+	mat4x4_perspective(projection,
+		60.f * (float)M_PI / 180.f,
+		ratio,
+		1.f, 1024.f);
+	glLoadMatrixf((const GLfloat*)projection);
+}
+
+
+//========================================================================
+// main
+//========================================================================
+
+int main(int argc, char* argv[])
+{
+	GLFWwindow* window;
+	double t, dt_total, t_old;
+	int width, height;
+
+	glfwSetErrorCallback(error_callback);
+
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+
+	window = glfwCreateWindow(640, 480, "Wave Simulation", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetMouseButtonCallback(window, mouse_button_callback);
+	glfwSetCursorPosCallback(window, cursor_position_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	glfwMakeContextCurrent(window);
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	glfwSwapInterval(1);
+
+	glfwGetFramebufferSize(window, &width, &height);
+	framebuffer_size_callback(window, width, height);
+
+	// Initialize OpenGL
+	init_opengl();
+
+	// Initialize simulation
+	init_vertices();
+	init_grid();
+	adjust_grid();
+
+	// Initialize timer
+	t_old = glfwGetTime() - 0.01;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		t = glfwGetTime();
+		dt_total = t - t_old;
+		t_old = t;
+
+		// Safety - iterate if dt_total is too large
+		while (dt_total > 0.f)
+		{
+			// Select iteration time step
+			dt = dt_total > MAX_DELTA_T ? MAX_DELTA_T : dt_total;
+			dt_total -= dt;
+
+			// Calculate wave propagation
+			calc_grid();
+		}
+
+		// Compute height of each vertex
+		adjust_grid();
+
+		// Draw wave grid to OpenGL display
+		draw_scene(window);
+
+		glfwPollEvents();
+	}
+
+	exit(EXIT_SUCCESS);
 }
